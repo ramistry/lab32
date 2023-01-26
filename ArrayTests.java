a69c67da47ea04917b43960fcbf78a67ce4ed63f
ramistry <122575713+ramistry@users.noreply.github.com>
2023-01-26 15:40:45 -0800

    Add files via upload

---
+++ ArrayTests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests {
+	@Test 
+	public void testReverseInPlace() {
+    int[] input2 = {1,2,3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {3,2,1}, input2);
+	}
+
+
+  @Test
+  public void testReversed() {
+    int[] input1 = { };
+    int[] input2 = {1,2,3};
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input2));
+  }
+}
